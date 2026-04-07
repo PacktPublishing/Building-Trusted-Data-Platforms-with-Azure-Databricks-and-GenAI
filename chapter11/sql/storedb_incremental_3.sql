7e437a2d697b2bc9564bf2b14c654feb13e2d25f
Manoj Kukreja <mkukreja1@gmail.com>
2026-04-07 15:48:22 -0400

    chapter11 commit

---
+++ chapter11/sql/storedb_incremental_3.sql
@@ -0,0 +1,5 @@
+INSERT INTO store_customers([customer_id],[customer_name],[address],[city],[postalcode],[country],[phone],[email],[credit_card]) 
+VALUES(4001,'Dr. Ella Solomon','Apartment #980-3873 Sed Av','Charlottetown','P0M','Canada','943-5925','adipiscing@gmail.com','5343300510782047'),
+(4002,'El Solomon','Ap #980-3873 Sed Av','Charlottetown','','Canada','1 (343) 943-5925','adipiscing@yahoo.com','5154959880757059');
+GO
+
